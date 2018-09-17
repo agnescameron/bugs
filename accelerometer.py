1d9065abaa3d36914848b4a2ee718082766a6805
Agnes Cameron <agnesfcameron@gmail.com>
2018-09-17 12:42:46 -0400

    accelerometer detects perturbation

--- accelerometer.py
+++ accelerometer.py
@@ -1,4 +1,5 @@
 import smbus
+import signal
 
 DEVICE_BUS = 1
 DEVICE_ADDR = 0x18
@@ -13,12 +14,17 @@ LIS3DH_DATARATE_400_HZ = 0x0b0111
 bus = smbus.SMBus(DEVICE_BUS)
 
 
+def handler(signum, frame):
+  print 'signal handler called', signum
+
 def twos_comp(val, bits):
-    if (val & (1 << (bits - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
-        val = val - (1 << bits)        # compute negative value
-    return val                         # return positive value as is
+  if (val & (1 << (bits - 1))) != 0: # if sign bit is set e.g., 8bit: 128-255
+      val = val - (1 << bits)        # compute negative value
+  return val                         # return positive value as is
 
 # setup 
+
+
 # enable all axes, normal mode, set to 400hz
 bus.write_byte_data(DEVICE_ADDR, LIS3DH_REG_CTRL1, 0x77)
 
@@ -31,6 +37,9 @@ bus.write_byte_data(DEVICE_ADDR, LIS3DH_REG_CTRL3, 0x10)
 #enable adcs
 bus.write_byte_data(DEVICE_ADDR, LIS3DH_REG_TEMPCFG, 0x80)
 
+prev_x = 0
+prev_y = 0
+prev_z = 0
 
 #bus.write_byte_data(DEVICE_ADDR, 0x31, 0x30)
 while True:
@@ -38,9 +47,25 @@ while True:
   for i in range(0x28, 0x2E):
     byte = bus.read_byte_data(DEVICE_ADDR, i)
     raw_values.append(byte)
-  x_result = (raw_values[1] << 4) + raw_values[0]
-  x_result_8 = (raw_values[1] << 8) + raw_values[0]
-  x_two_comp = twos_comp(int(x_result_8), 16)
-  y_result = (raw_values[3] << 4) + raw_values[2]
-  z_result = (raw_values[5] << 4) + raw_values[4]
-  print x_result, "  ", x_two_comp, "   ",  y_result, "  ", z_result
+  x_result = (raw_values[1] << 8) + raw_values[0]
+  y_result = (raw_values[3] << 8) + raw_values[2]
+  z_result = (raw_values[5] << 8) + raw_values[4]
+  x_two_comp = twos_comp(int(x_result), 16)
+  y_two_comp = twos_comp(int(y_result), 16)
+  z_two_comp = twos_comp(int(z_result), 16)
+  #print x_two_comp, "   ",  y_two_comp, "  ", z_two_comp
+  if (prev_x == 0) and (prev_y == 0) and (prev_z ==0):
+    delta_x = 0
+    delta_y = 0
+    delta_z = 0
+  else:
+    delta_x = x_two_comp - prev_x
+    delta_y = y_two_comp - prev_y
+    delta_z = z_two_comp - prev_z
+  prev_x = x_two_comp
+  prev_y = y_two_comp
+  prev_z = z_two_comp
+
+  avg_delta = (delta_x + delta_y + delta_z) / 3
+  if avg_delta > 1000:
+    print "deltas  ", delta_x, "    ", delta_y, "    ", delta_z
